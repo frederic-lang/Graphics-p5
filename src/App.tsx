1da5f0b113678f78f60eef474e38e5a859db067a
Frédéric Lang <f.lang@hotmail.fr>
2023-05-01 18:52:48 +0200

    first Sketch

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { MySketch } from './Sketch'
+
+function App() {
+
+  return (
+    <>
+      <h1>Graphics</h1>
+      <div className="card">
+        <MySketch />
+      </div>
+      <p className="read-the-docs">
+        made with p5.js
+      </p>
+    </>
+  )
+}
+
+export default App
